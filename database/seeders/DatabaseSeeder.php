2c1057a721f937fead2802b071357c9ed00798c0
Lena <lena.schoenhoff@urs-os.de>
2022-06-16 14:45:27 +0200

    anfang .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        DB::table('teams')->insert(['name' => 'Deutschland', 'gruppe' => 'A' ]); 
+        DB::table('teams')->insert(['name' => 'Deutschland', 'gruppe' => 'A' ]);
+        DB::table('teams')->insert(['name' => 'Italien', 'gruppe' => 'A' ]); 
     }
 }
