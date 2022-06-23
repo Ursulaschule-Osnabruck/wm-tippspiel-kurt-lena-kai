31dad3f5e7e3653d454e706950978c0ed4c0ffbc
Lena <lena.schoenhoff@urs-os.de>
2022-06-23 14:06:20 +0200

    23.06.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,37 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         DB::table('teams')->insert(['name' => 'Deutschland', 'gruppe' => 'A' ]);
-        DB::table('teams')->insert(['name' => 'Italien', 'gruppe' => 'A' ]); 
+        DB::table('teams')->insert(['name' => 'Italien', 'gruppe' => 'A' ]);
+        DB::table('teams')->insert(['name' => 'Katar', 'gruppe' => 'A' ]);
+        DB::table('teams')->insert(['name' => 'Dänemark', 'gruppe' => 'A' ]);
+        DB::table('teams')->insert(['name' => 'Brasilien', 'gruppe' => 'B' ]); 
+        DB::table('teams')->insert(['name' => 'Frankreich', 'gruppe' => 'B' ]); 
+        DB::table('teams')->insert(['name' => 'Belgien', 'gruppe' => 'B' ]); 
+        DB::table('teams')->insert(['name' => 'Kroatien', 'gruppe' => 'B' ]); 
+        DB::table('teams')->insert(['name' => 'Serbien', 'gruppe' => 'C' ]); 
+        DB::table('teams')->insert(['name' => 'Spanien', 'gruppe' => 'C' ]); 
+        DB::table('teams')->insert(['name' => 'England', 'gruppe' => 'C' ]); 
+        DB::table('teams')->insert(['name' => 'Schweiz', 'gruppe' => 'C' ]); 
+        DB::table('teams')->insert(['name' => 'Niederlande', 'gruppe' => 'D' ]); 
+        DB::table('teams')->insert(['name' => 'Argentinien', 'gruppe' => 'D' ]); 
+        DB::table('teams')->insert(['name' => 'Iran', 'gruppe' => 'D' ]); 
+        DB::table('teams')->insert(['name' => 'Südkorea', 'gruppe' => 'D' ]); 
+        DB::table('teams')->insert(['name' => 'Saudi-Arabien', 'gruppe' => 'E' ]);
+        DB::table('teams')->insert(['name' => 'Japan', 'gruppe' => 'E' ]); 
+        DB::table('teams')->insert(['name' => 'Ecuador', 'gruppe' => 'E' ]); 
+        DB::table('teams')->insert(['name' => 'Uruguay', 'gruppe' => 'E' ]); 
+        DB::table('teams')->insert(['name' => 'Kanada', 'gruppe' => 'F' ]); 
+        DB::table('teams')->insert(['name' => 'Ghana', 'gruppe' => 'F' ]); 
+        DB::table('teams')->insert(['name' => 'Senegal', 'gruppe' => 'F' ]); 
+        DB::table('teams')->insert(['name' => 'Portugal', 'gruppe' => 'F' ]); 
+        DB::table('teams')->insert(['name' => 'Polen', 'gruppe' => 'G' ]); 
+        DB::table('teams')->insert(['name' => 'Marokko', 'gruppe' => 'G' ]); 
+        DB::table('teams')->insert(['name' => 'Tunesien', 'gruppe' => 'G' ]); 
+        DB::table('teams')->insert(['name' => 'Kamerun', 'gruppe' => 'G' ]); 
+        DB::table('teams')->insert(['name' => 'Mexiko', 'gruppe' => 'H' ]); 
+        DB::table('teams')->insert(['name' => 'USA', 'gruppe' => 'H' ]); 
+        DB::table('teams')->insert(['name' => 'Wales', 'gruppe' => 'H' ]); 
+        DB::table('teams')->insert(['name' => 'Australien', 'gruppe' => 'H' ]); 
+        DB::table('teams')->insert(['name' => 'Costa Rica', 'gruppe' => 'H' ]); 
     }
 }
